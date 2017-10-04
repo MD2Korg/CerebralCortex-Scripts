e29dbb4c0e3c522480843d56c18d3b8503167ba1
nasirali1 <nasir.ali08@gmail.com>
2017-10-04 14:31:17 -0500

    added command line argument to replay-data

--- data_replay/replay_data.py
+++ data_replay/replay_data.py
@@ -22,51 +22,85 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from kafka import KafkaProducer
-import json
+import glob
+import os
 import sys
-import os; import glob
-producer = KafkaProducer(bootstrap_servers='localhost:9092', api_version=(0,10), value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+import json
+
+from kafka import KafkaProducer
+
 
 class ReplayCerebralCortexData:
     def __init__(self, start_time=None, end_time=None):
+        """
+        Constructor
+        :param configuration:
+        """
+        # python3 replay_data.py 127.0.0.1:9092 data-folder-path start-time(opt) end-time(opt)
+
         if not sys.argv[1]:
             raise ValueError("Missing Kafka broker URL/IP and Port.")
         elif not sys.argv[2]:
             raise ValueError("Missing data directory path.")
 
-        self.start_time = start_time
-        self.end_time = end_time
+        try:
+            if sys.argv[3]:
+                self.start_time = start_time
+            elif sys.argv[4]:
+                self.end_time = end_time
+        except:
+            self.start_time = ""
+            self.end_time = ""
+
         self.kafka_broker = sys.argv[1]
         self.data_dir = sys.argv[2]
-        self.producer = KafkaProducer(bootstrap_servers=self.kafka_broker, api_version=(0,10), value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+        self.producer = KafkaProducer(bootstrap_servers=self.kafka_broker, api_version=(0, 10),
+                                      value_serializer=lambda v: json.dumps(v).encode('utf-8'))
         self.read_data_dir()
 
     def publish_filequeue(self, metadata, filename):
-        self.producer("filequeue", {"metadata":metadata, "filename":filename})
+        self.producer("filequeue", {"metadata": metadata, "filename": filename})
 
     def read_data_dir(self):
 
         self.data_dir = self.data_dir
-        files = list(filter(os.path.isfile, glob.glob(self.data_dir + "*.gz")))
+        filenames = list(filter(os.path.isfile, glob.glob(self.data_dir + "*.gz")))
         if self.start_time or self.end_time:
-            files = list(filter(lambda x: self.filter_filenames(x), files))
-        files.sort(key=lambda x: os.path.getmtime(x))
+            filenames = list(filter(lambda x: self.filter_filenames(x), filenames))
+        filenames.sort(key=lambda x: os.path.getmtime(x))
 
-        # dd=max([os.path.join(self.data_dir,d) for d in os.listdir(self.data_dir)], key=os.path.getmtime)
-        # print(dd)
+        self.produce_kafka_message(filenames)
 
-    def filter_filenames(self, file_name, start_time, end_time):
+    def filter_filenames(self, file_name):
         if self.start_time and not self.end_time:
-            if os.path.getmtime(file_name)>=start_time:
+            if os.path.getmtime(file_name) >= self.start_time:
                 return file_name
         elif not self.start_time and self.end_time:
-            if os.path.getmtime(file_name)<=end_time:
+            if os.path.getmtime(file_name) <= self.end_time:
                 return file_name
         elif self.start_time and self.end_time:
-            if os.path.getmtime(file_name)>=start_time and os.path.getmtime(file_name)<=end_time:
+            if os.path.getmtime(file_name) >= self.start_time and os.path.getmtime(file_name) <= self.end_time:
                 return file_name
         else:
-                return file_name
+            return file_name
 
-ReplayCerebralCortexData(start_time= 1506979290.049632, end_time= 1506979309.049632)
+    def produce_kafka_message(self, filenames):
+        producer = KafkaProducer(bootstrap_servers=self.kafka_broker, api_version=(0, 10),
+                                 value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+        for filename in filenames:
+            metadata_filename = filename.replace(".gz", ".json")
+            data_filename = filename.replace(self.data_dir, "")
+
+            metadata_file = open(metadata_filename, 'r')
+            metadata = metadata_file.read()
+            try:
+                metadata = json.loads(metadata)
+            except:
+                metadata = metadata
+            producer.send("filequeue", {"metadata": metadata, "filename": data_filename})
+            producer.flush()
+            metadata_file.close()
+            print("Yielding file:", metadata_filename, data_filename)
+
+
+ReplayCerebralCortexData(start_time=1506979290.049632, end_time=1506979309.049632)

fabfb535fe6d95b7c69d90fc8e7067456bfa1276
nasirali1 <nasir.ali08@gmail.com>
2018-02-08 11:41:11 -0600

    changed path

--- data_replay/run.sh
+++ data_replay/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python3 replay_data.py -b "dagobah10dot.memphis.edu:9092" -d "/cerebralcortex/apiserver/data/00ab666c-afb8-476e-9872-6472b4e66b68/20171224/"
+python3 replay_data.py -b "dagobah10dot.memphis.edu:9092" -d "/cerebralcortex/apiserver/data/"

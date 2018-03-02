6e32edaf641326be8e65a1ce9f9537a6d98cf153
nasirali1 <nasir.ali08@gmail.com>
2018-03-02 12:18:07 -0600

    merged CC and script configs

---
+++ data_replay/run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+python3 main.py -conf "/home/ali/IdeaProjects/CerebralCortex-2.0/cerebralcortex/core/resources/cc_configuration.yml"
--- data_replay/shell_scripts_legacy/run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-python3 replay_data_using_kafka.py -b "127.0.0.1:9092" -d "/home/ali/IdeaProjects/MD2K_DATA/data/" -uid "00000000-3248-4b7d-b4f6-8593b4faaa63"

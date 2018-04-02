dfd2a5c43c41b6491c399a971222d994c2bf7e8b
nasirali1 <nasir.ali08@gmail.com>
2018-04-02 18:31:52 -0500

    fixed a typo

--- data_compress/run.sh
+++ data_compress/run.sh
@@ -2,4 +2,4 @@ CC_CONFIG="/cerebralcortex/code/ali/cc_config/cc_configuration.yml"
 START=0
 END=1
 
-sh comp.py -conf $CC_CONFIG -start $START -end $END
+python3.6 comp.py -conf $CC_CONFIG -start $START -end $END

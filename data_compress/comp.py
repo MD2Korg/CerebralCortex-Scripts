76cd8331c9655392fa366ed965ea82d315804354
nasirali1 <nasir.ali08@gmail.com>
2018-04-02 19:29:40 -0500

    moved CC save_stream code to comp.py because of double hdfs connection error

--- data_compress/comp.py
+++ data_compress/comp.py
@@ -1,9 +1,14 @@
 import pyarrow
 import argparse
+import pickle
+import gzip
+import traceback
+from cerebralcortex.core.datatypes.datapoint import DataPoint
+from cerebralcortex.core.datatypes.datastream import DataStream
 from cerebralcortex.cerebralcortex import CerebralCortex
 
 
-def compress_hdfs_data(CC, start, end):
+def compress_hdfs_data(start, end):
     """
 
     :param CC: CerebralCortex object
@@ -25,11 +30,68 @@ def compress_hdfs_data(CC, start, end):
             for day_file in day_files:
                 if day_file[-3:] != ".gz":
                     tmp = day_file.split("/")
-                    owner_id = tmp[3]
-                    stream_id = tmp[4]
-                    day = tmp[5].replace(".pickle", "")
-                    CC.get_stream(stream_id, owner_id, day)
+                    # owner_id = tmp[3]
+                    # stream_id = tmp[4]
+                    # day = tmp[5].replace(".pickle", "")
+                    print(day_file)
+                    process_file(hdfs, day_file)
+                    exit(1)
 
+def process_file(hdfs, filename):
+    data = None
+    try:
+        if hdfs.exists(filename):
+            with hdfs.open(filename, "rb") as curfile:
+                data = curfile.read()
+        else:
+            print(filename.replace("pickle",""), "does not exist.")
+
+        if data is not None and data!=b'':
+            data = pickle.loads(data)
+            clean_data = filter_sort_datapoints(data)
+            compress_store_pickle(filename, clean_data,hdfs)
+    except Exception as e:
+        print("Error processing:", filename, "Error: ", str(traceback.format_exc()))
+
+def filter_sort_datapoints(data):
+    if len(data)>0:
+        clean_data = dedup(sorted(data))
+        return clean_data
+    else:
+        return data
+
+def dedup(data):
+    result = [data[0]]
+    for dp in data[1:]:
+        if dp.start_time == result[-1].start_time:
+            continue
+        result.append(dp)
+
+    return result
+def compress_store_pickle(filename: str, data: pickle, hdfs: object=None):
+    """
+
+    :param filename: pickle file name
+    :param data: pickled data
+    :param hdfs: hdfs connection object
+    """
+    gz_filename = filename.replace(".pickle", ".gz")
+    if len(data)>0:
+        data = pickle.dumps(data)
+        compressed_data = gzip.compress(data)
+
+        try:
+            if not hdfs.exists(gz_filename):
+                with hdfs.open(gz_filename, "wb") as gzwrite:
+                    gzwrite.write(compressed_data)
+            if hdfs.exists(filename):
+                if hdfs.info(gz_filename)["size"]>0:
+                    hdfs.delete(filename)
+        except:
+            print("Error in generating gz file.")
+            # delete file if file was opened and no data was written to it
+            if hdfs.info(gz_filename)["size"]==0:
+                hdfs.delete(gz_filename)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='CerebralCortex Data Compress Script')
@@ -39,6 +101,6 @@ if __name__ == "__main__":
 
     args = vars(parser.parse_args())
 
-    if args["conf"] is not None and args["conf"] != "":
-        CC = CerebralCortex(args["conf"])
-        compress_hdfs_data(CC, args["start"], args["end"])
+    # if args["conf"] is not None and args["conf"] != "":
+    #     CC = CerebralCortex(args["conf"])
+    compress_hdfs_data(args["start"], args["end"])

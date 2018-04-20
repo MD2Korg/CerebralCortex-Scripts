f4bcdd759e9a1602a18973397ea981b7fe128a1a
ali <asdf@asdf.com>
2018-04-19 19:18:52 -0500

    changed how stream uuid is generated

--- environment_data/mysql_to_stream.py
+++ environment_data/mysql_to_stream.py
@@ -81,6 +81,9 @@ class SqlToCCStream():
                                     output_stream_id = str(metadata["data_descriptor"])+str(execution_context)+str(metadata["annotations"])
                                     output_stream_id += "weather-data-stream"
                                     output_stream_id += "weather-data-stream"
+                                    output_stream_id += str(uid)
+                                    output_stream_id += str(sid)
+                                    # output_stream_id += str(day)
                                     output_stream_id = str(uuid.uuid3(uuid.NAMESPACE_DNS, output_stream_id))
                                     ds = DataStream(identifier=output_stream_id, owner=uid, name=metadata["name"], data_descriptor=metadata["data_descriptor"], execution_context=execution_context, annotations=metadata["annotations"], stream_type=metadata["type"], data=dps)
 

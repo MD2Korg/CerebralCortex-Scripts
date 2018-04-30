8281bd6b4294557ff550114fdf8984c6970af13e
ali <asdf@asdf.com>
2018-04-30 11:30:23 -0500

    updated run to scan for only selected participants

--- data_replay/run.sh
+++ data_replay/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python3 main.py -conf "/home/ali/IdeaProjects/CerebralCortex-DockerCompose/cc_config_file/cc_vagrant_configuration.yml"
+python3.6 mysql_to_stream.py -users selected -conf /cerebralcortex/code/ali/cc_config/cc_configuration.yml
